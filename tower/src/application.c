3d7a39dbaa410816ff495b9e9b9dcdbdc3af65c0
Adam Krejza <adamfkrejza@gmail.com>
2026-02-27 23:22:26 +0100

    Add draw_pixel function

--- tower/src/application.c
+++ tower/src/application.c
@@ -4,6 +4,8 @@
 
 #include <application.h>
 
+void draw_pixel(uint16_t row, uint16_t col, uint16_t color);
+void reset_screen();
 
 // Application initialization function which is called once after boot
 void application_init(void)
@@ -71,51 +73,8 @@ void application_init(void)
     uint8_t madctl = 0x48;   // typical: MX | BGR
     twr_spi_transfer(&madctl, NULL, 1);
     twr_gpio_set_output(TWR_GPIO_P15, 1);
-
-
-
-
-
-
-    // set the screen to black
-    uint16_t width = 240;
-    uint16_t height = 320;
-    cmd = 0x2A; // column address
-    twr_gpio_set_output(TWR_GPIO_P15, 0);
-    twr_gpio_set_output(TWR_GPIO_P0, 0);
-    twr_spi_transfer(&cmd, NULL, 1);
-    twr_gpio_set_output(TWR_GPIO_P0, 1);
-    uint8_t col_data[4] = {0,0,(width-1)>>8,(width-1)&0xFF};
-    twr_spi_transfer(col_data, NULL, 4);
-    twr_gpio_set_output(TWR_GPIO_P15, 1);
-
-    cmd = 0x2B; // row address
-    twr_gpio_set_output(TWR_GPIO_P15, 0);
-    twr_gpio_set_output(TWR_GPIO_P0, 0);
-    twr_spi_transfer(&cmd, NULL, 1);
-    twr_gpio_set_output(TWR_GPIO_P0, 1);
-    uint8_t row_data[4] = {0,0,(height-1)>>8,(height-1)&0xFF};
-    twr_spi_transfer(row_data, NULL, 4);
-    twr_gpio_set_output(TWR_GPIO_P15, 1);
-
-    cmd = 0x2C; // write memory
-    twr_gpio_set_output(TWR_GPIO_P15, 0);
-    twr_gpio_set_output(TWR_GPIO_P0, 0);
-    twr_spi_transfer(&cmd, NULL, 1);
-    twr_gpio_set_output(TWR_GPIO_P0, 1);
-
-    // fill pixels with black
-    uint8_t pixels[2] = {0x00, 0x00};
-    for(uint32_t i = 0; i < width*height; i++)
-    {
-        twr_spi_transfer(pixels, NULL, 2);
-    }
-    twr_gpio_set_output(TWR_GPIO_P15, 1);
-    /*	-	-	-	-	-	-	-	-	-	-*/
-
-
-
-
+	
+	reset_screen();
 
     // turn display on (it flickers)
     cmd = 0x29;
@@ -125,7 +84,7 @@ void application_init(void)
     twr_gpio_set_output(TWR_GPIO_P15, 1);
 
 
-    // set address window
+    // set address window and print a pixel
 
     // column address
     uint16_t x = 150;
@@ -193,7 +152,26 @@ void application_init(void)
     twr_spi_transfer(pixel, NULL, 2);
     twr_gpio_set_output(TWR_GPIO_P15, 1);
 
+	for (short i = 0; i < 240; i++)
+	{
+		draw_pixel(i, 5, 0x67f);
+	}
 
+	/* amogus:
+	66, 4
+
+
+	*/
+
+
+	// very slowly prints a square
+	// for (int i = 0; i < 127; i++)
+	// {
+	// 	for (int j = 0; j < 127; j++)
+	// 	{
+	// 		draw_pixel(i, j, 0xff00); // red // TODO: make an 'enum' or some mapping of colors to hex codes
+	// 	}
+	// }
     
     // twr_log_debug("%d, %d\n", (int)twr_gpio_get_mode(TWR_GPIO_P0), (int)twr_gpio_get_output(TWR_GPIO_P0));
 }
@@ -211,4 +189,100 @@ void application_task(void)
     // toggles from LOW to HIGH
     // twr_gpio_get_mode(TWR_GPIO_P0) == 1 ? twr_gpio_set_mode(TWR_GPIO_P0, TWR_GPIO_MODE_INPUT) : twr_gpio_set_mode(TWR_GPIO_P0, TWR_GPIO_MODE_OUTPUT);
     // twr_log_debug("%d, %d\n", (int)twr_gpio_get_mode(TWR_GPIO_P0), (int)twr_gpio_get_output(TWR_GPIO_P0));
+}
+
+// https://hello.lumiere-couleur.com/app/16bit-colorpicker/
+// row 
+void draw_pixel(uint16_t row, uint16_t col, uint16_t color)	// TODO: what the fuck am i doing
+{
+	uint8_t cmd;
+
+    // set column address
+	cmd = 0x2A; 
+    twr_gpio_set_output(TWR_GPIO_P15, 0);
+    twr_gpio_set_output(TWR_GPIO_P0, 0);
+    twr_spi_transfer(&cmd, NULL, 1);
+    twr_gpio_set_output(TWR_GPIO_P0, 1);
+    uint8_t col_data[4] =
+    { // TODO: review bitwise AND and bitshifting
+        col >> 8,
+		col & 0xff,
+        col << 8,
+		col & 0xff
+    };
+    twr_spi_transfer(col_data, NULL, 4);
+    twr_gpio_set_output(TWR_GPIO_P15, 1);
+
+	// set row address
+    cmd = 0x2B; 
+    twr_gpio_set_output(TWR_GPIO_P15, 0);
+    twr_gpio_set_output(TWR_GPIO_P0, 0);
+    twr_spi_transfer(&cmd, NULL, 1);
+    twr_gpio_set_output(TWR_GPIO_P0, 1);
+
+    uint8_t row_data[4] =
+    { // TODO: review bitwise AND and bitshifting
+        row >> 8,
+		row & 0xff,
+        row << 8,
+		row & 0xff
+    };
+    twr_spi_transfer(row_data, NULL, 4);
+    twr_gpio_set_output(TWR_GPIO_P15, 1);
+
+
+	// write 1 pixel
+    cmd = 0x2C; 
+    twr_gpio_set_output(TWR_GPIO_P15, 0);
+    twr_gpio_set_output(TWR_GPIO_P0, 0);
+    twr_spi_transfer(&cmd, NULL, 1);
+    twr_gpio_set_output(TWR_GPIO_P0, 1);
+
+	// sets color
+    uint8_t pixels[2] = {
+		color >> 8,
+		color & 0xff
+	};
+
+	twr_spi_transfer(pixels, NULL, 2);
+    twr_gpio_set_output(TWR_GPIO_P15, 1);
+}
+
+// sets all pixels black
+void reset_screen()
+{
+	uint16_t width = 240;
+    uint16_t height = 320;
+    uint8_t cmd = 0x2A; // column address
+
+    twr_gpio_set_output(TWR_GPIO_P15, 0);
+    twr_gpio_set_output(TWR_GPIO_P0, 0);
+    twr_spi_transfer(&cmd, NULL, 1);
+    twr_gpio_set_output(TWR_GPIO_P0, 1);
+    uint8_t col_data[4] = {0,0,(width-1)>>8,(width-1)&0xFF};
+    twr_spi_transfer(col_data, NULL, 4);
+    twr_gpio_set_output(TWR_GPIO_P15, 1);
+
+    cmd = 0x2B; // row address
+    twr_gpio_set_output(TWR_GPIO_P15, 0);
+    twr_gpio_set_output(TWR_GPIO_P0, 0);
+    twr_spi_transfer(&cmd, NULL, 1);
+    twr_gpio_set_output(TWR_GPIO_P0, 1);
+    uint8_t row_data[4] = {0,0,(height-1)>>8,(height-1)&0xFF};
+    twr_spi_transfer(row_data, NULL, 4);
+    twr_gpio_set_output(TWR_GPIO_P15, 1);
+
+    cmd = 0x2C; // write memory
+    twr_gpio_set_output(TWR_GPIO_P15, 0);
+    twr_gpio_set_output(TWR_GPIO_P0, 0);
+    twr_spi_transfer(&cmd, NULL, 1);
+    twr_gpio_set_output(TWR_GPIO_P0, 1);
+
+    // fill pixels with black
+    uint8_t pixels[2] = {0x00, 0x00};
+    for(uint32_t i = 0; i < width*height; i++)
+    {
+        twr_spi_transfer(pixels, NULL, 2);
+    }
+    twr_gpio_set_output(TWR_GPIO_P15, 1);
 }
