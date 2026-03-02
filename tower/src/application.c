b2ce55125a49469336ffa0f0b6f225e7366d4dcd
Adam Krejza <adamfkrejza@gmail.com>
2026-03-02 13:12:48 +0100

    Code cleanup

--- tower/src/application.c
+++ tower/src/application.c
@@ -65,8 +65,7 @@ void application_task(void)
     twr_scheduler_plan_current_from_now(1000);
 }
 
-// https://hello.lumiere-couleur.com/app/16bit-colorpicker/
-// row 
+// TODO: make this a draw_rect wrapper
 void draw_pixel(uint16_t row, uint16_t col, uint16_t color)
 {
 	uint8_t cmd;
@@ -74,7 +73,7 @@ void draw_pixel(uint16_t row, uint16_t col, uint16_t color)
 	set_address(SET_COL, col, col);
 	set_address(SET_ROW, row, row);
 
-    cmd = 0x2C; // write memory
+    cmd = 0x2C;
     command(cmd);
 
 	// sets color
@@ -131,7 +130,7 @@ void reset_background()
 // TODO: document set_address, start_pixel_stream, and command
 
 // axis is either SET_COL or SET_ROW (0x2A or 0x2B)
-void set_address(uint8_t axis, uint16_t start, uint16_t end) // add start and end since right now it only works for one pixel
+void set_address(uint8_t axis, uint16_t start, uint16_t end)
 {
 	command(axis);
     uint8_t axis_range[4] =
@@ -145,7 +144,6 @@ void set_address(uint8_t axis, uint16_t start, uint16_t end) // add start and en
     twr_gpio_set_output(TWR_GPIO_P15, 1);
 }
 
-// every 2 bytes are interpreted as 1 pixel
 void start_pixel_stream()
 {
 	uint8_t cmd = 0x2C;
@@ -266,8 +264,7 @@ void draw_rect(uint16_t col_start, uint16_t col_end, uint16_t row_start, uint16_
 
     uint16_t width  = (col_end - col_start + 1);
     uint16_t height = (row_end - row_start + 1);
-	uint32_t pixel_count = width * height;
-    uint32_t byte_count = 2 * width * height;
+    uint32_t byte_count = 2 * width * height; // 16 bit colors
 
     uint16_t chunk_count = byte_count / BUFFER_SIZE;
     uint16_t remainder = byte_count % BUFFER_SIZE;
@@ -285,50 +282,6 @@ void draw_rect(uint16_t col_start, uint16_t col_end, uint16_t row_start, uint16_
 
     twr_gpio_set_output(TWR_GPIO_P15, 1);
 }
-
-// void draw_rect(uint16_t col_start, uint16_t col_end, uint16_t row_start, uint16_t row_end, uint16_t color)
-// {
-// 	set_address(SET_COL, col_start, col_end);
-// 	set_address(SET_ROW, row_start, row_end);
-//     start_pixel_stream();
-
-//     uint8_t color_data[2] = { // most then least significant
-// 		color >> 8,
-// 		color & 0xFF
-// 	};
-
-//     uint16_t width  = (col_end - col_start + 1);
-//     uint16_t height = (row_end - row_start + 1);
-// 	uint32_t pixel_count = width * height;
-//     uint32_t byte_count = 2 * width * height;
-
-//     uint16_t chunk_count = byte_count / BUFFER_SIZE;
-//     uint16_t remainder = byte_count % BUFFER_SIZE;
-//     uint8_t buffer[BUFFER_SIZE];
-
-//     for (uint16_t i = 0; i < BUFFER_SIZE; i += 2) {
-//         buffer[i] = color >> 8;
-//         buffer[i + 1] = color & 0xFF;
-//     }
-
-// 	twr_spi_transfer(color_data, NULL, 2);
-// 	twr_spi_transfer(color_data, NULL, 2);
-// 	twr_spi_transfer(color_data, NULL, 2);
-// 	twr_spi_transfer(color_data, NULL, 2);
-// 	twr_spi_transfer(color_data, NULL, 2);
-
-//     for (uint16_t i = 0; i < chunk_count; i++)
-// 		twr_spi_transfer(buffer, NULL, BUFFER_SIZE);
-//     for (uint16_t i = 0; i < remainder; i += 2)
-// 		twr_spi_transfer(color_data, NULL, 2);
-
-
-// 	// for (uint32_t i = 0; i < pixel_count; i++)
-// 	// 	twr_spi_transfer(color_data, NULL, 2);
-
-//     twr_gpio_set_output(TWR_GPIO_P15, 1);
-// }
-
 
 // idk if this is any better than draw_rect tbh
 // void draw_line(uint16_t col, uint16_t row, uint8_t axis, uint8_t length, uint16_t color)
