d76e6b4f9288be3e56274fec0fd2da77fa74e6ab
Adam Krejza <adamfkrejza@gmail.com>
2026-03-10 16:04:34 +0100

    Clean up GPIO, simplify function names, add wrappers

--- tower/src/display.c
+++ tower/src/display.c
@@ -16,70 +16,79 @@
 const uint16_t BG_COLOR = BLACK;
 const uint16_t TEXT_COLOR = WHITE;
 
-// commands for set_address()
-const uint8_t SET_COL = 0x2A;
-const uint8_t SET_ROW = 0x2B;
-
 uint8_t buffer[BUFFER_SIZE];
 
 void clear_char(uint16_t grid_x, uint16_t grid_y, uint8_t text_size);
-static void command(uint8_t cmd);
 void display_init();
 void draw_char(unsigned char c, uint16_t grid_x, uint16_t grid_y, uint8_t text_size);
 void draw_image(uint16_t col, uint16_t row , uint8_t type);
 void draw_pixel(uint16_t row, uint16_t col, uint16_t color);
 void draw_rect(uint16_t col_start, uint16_t col_end, uint16_t row_start, uint16_t row_end, uint16_t color);
+static inline void lcd_command(uint8_t cmd);
+static inline void lcd_pixel_stream();
+static inline void lcd_set_address(uint8_t axis, uint16_t start, uint16_t end);
+static inline void lcd_set_col(uint16_t start, uint16_t end);
+static inline void lcd_set_row(uint16_t start, uint16_t end);
+static inline void lcd_send_data(void *source, void *destination, size_t length);
 void outline_screen(const uint16_t color);
 void paint_screen(uint16_t color);
-static void set_address(uint8_t axis, uint16_t start, uint16_t end);
-static void start_pixel_stream();
 
-// axis is either SET_COL or SET_ROW (0x2A or 0x2B)
 // defines the memory region it'll write to
-static inline void set_address(uint8_t axis, uint16_t start, uint16_t end)
+static inline void lcd_set_address(uint8_t axis, uint16_t start, uint16_t end)
 {
-	command(axis);
+	lcd_command(axis);
 	uint8_t axis_range[4] = {
 		start >> 8,
 		start & 0xFF,
 		end >> 8,
 		end & 0xFF
 	};
-	twr_spi_transfer(axis_range, NULL, 4);
-	twr_gpio_set_output(PIN_CS, 1);
+	lcd_send_data(axis_range, NULL, 4);
 }
 
-static inline void start_pixel_stream()
+static inline void lcd_set_col(uint16_t start, uint16_t end)
+{
+	lcd_set_address(0x2A, start, end);
+}
+
+static inline void lcd_set_row(uint16_t start, uint16_t end)
+{
+	lcd_set_address(0x2B, start, end);
+}
+
+static inline void lcd_send_data(void *source, void *destination, size_t length)
+{
+	twr_gpio_set_output(PIN_CS, 0);
+	twr_gpio_set_output(PIN_DC, 1);
+	twr_spi_transfer(source, destination, length);
+}
+
+static inline void lcd_pixel_stream()
 {
 	uint8_t cmd = 0x2C;
-	command(cmd);
+	lcd_command(cmd);
 }
 
-static inline void command(uint8_t cmd)
+static inline void lcd_command(uint8_t cmd)
 {
 	twr_gpio_set_output(PIN_CS, 0);
 	twr_gpio_set_output(PIN_DC, 0);
 	twr_spi_transfer(&cmd, NULL, 1);
-	twr_gpio_set_output(PIN_DC, 1);
 }
 
-// TODO: add variables for CS, CD etc in case I change the pin layout
 // sets GPIO for SPI and starts the display
 void display_init()
 {
-	twr_gpio_init(PIN_CS); // CS
-	twr_gpio_init(PIN_DC);  // DC
-	twr_gpio_init(PIN_RST);  // RST
-	twr_gpio_init(PIN_MOSI); // SPI MOSI
-	twr_gpio_init(PIN_CLK); // CLK
+	twr_gpio_init(PIN_CS);
+	twr_gpio_init(PIN_DC);
+	twr_gpio_init(PIN_RST);
+	twr_gpio_init(PIN_MOSI);
+	twr_gpio_init(PIN_CLK);
 	
 	twr_gpio_set_mode(PIN_DC, TWR_GPIO_MODE_OUTPUT);
 	twr_gpio_set_mode(PIN_CS, TWR_GPIO_MODE_OUTPUT);
 	twr_gpio_set_mode(PIN_RST, TWR_GPIO_MODE_OUTPUT);
 
-	// DC default DATA
-	// set CS pin to output HIGH (idle)
-	// set RST HIGH
 	twr_gpio_set_output(PIN_DC, 1);
 	twr_gpio_set_output(PIN_CS, 1);
 	twr_gpio_set_output(PIN_RST, 1);
@@ -94,25 +103,20 @@ void display_init()
 
 	// sleep out
 	uint8_t cmd = 0x11;
-	command(cmd);
+	lcd_command(cmd);
 
 	twr_delay_us(62000); twr_delay_us(62000); // minimum 120ms for display start
 
 	// set it to 16-bit color mode
 	cmd = 0x3A;
-	twr_gpio_set_output(PIN_CS, 0);
-	twr_gpio_set_output(PIN_DC, 0);
-	twr_spi_transfer(&cmd, NULL, 1);
+	lcd_command(cmd);
 	uint8_t data = 0x55;
-	twr_gpio_set_output(PIN_DC, 1);
-	twr_spi_transfer(&data, NULL, 1);
-	twr_gpio_set_output(PIN_CS, 1);
+	lcd_send_data(&data, NULL, 1);
 
 	// set memory access control
 	cmd = 0x36;   // MADCTL
-	command(cmd);
+	lcd_command(cmd);
 
-	// TODO: Add more legible settings, this sucks
 	// Memory Data Access Control command for stuff like mirroring or flipping the display
 	uint8_t madctl = 0;
 	madctl = madctl + 1; // MY
@@ -127,12 +131,11 @@ void display_init()
 	madctl = madctl << 1;
 	madctl = madctl + 0; // MH
 	madctl = madctl << 2; // last 2 bits are unused
-	twr_spi_transfer(&madctl, NULL, 1);
-	twr_gpio_set_output(PIN_CS, 1);
+	lcd_send_data(&madctl, NULL, 1);
 
 	// turn display on
 	cmd = 0x29;
-	command(cmd);
+	lcd_command(cmd);
 }
 
 // draw rectangle, shape drawing primitive
@@ -147,13 +150,13 @@ void draw_rect(uint16_t col_start, uint16_t col_end, uint16_t row_start, uint16_
 		return;
 	}
 	if (col_start > col_end || row_start > row_end) {
-		twr_log_debug("Error in draw_rect: start greater than end"); // TODO: do bounds checking everywhere
+		twr_log_debug("Error in draw_rect: start greater than end"); // TODO: do bounds checking everywhere?
 		return;
 	}
 
 	uint16_t width  = (col_end - col_start + 1);
 	uint16_t height = (row_end - row_start + 1);
-	uint32_t byte_count = 2 * width * height; // 16 bit colors
+	uint32_t byte_count = 2 * width * height;
 
 	uint16_t chunk_count = byte_count / BUFFER_SIZE;
 	uint16_t remainder = byte_count % BUFFER_SIZE;
@@ -166,15 +169,13 @@ void draw_rect(uint16_t col_start, uint16_t col_end, uint16_t row_start, uint16_
 		buffer[i + 1] = color & 0xFF;
 	}
 
-	set_address(SET_COL, col_start, col_end);
-	set_address(SET_ROW, row_start, row_end);
-	start_pixel_stream();
+	lcd_set_col(col_start, col_end);
+	lcd_set_row(row_start, row_end);
+	lcd_pixel_stream();
 
 	for (uint16_t i = 0; i < chunk_count; i++)
-		twr_spi_transfer(buffer, NULL, BUFFER_SIZE);
-	twr_spi_transfer(buffer, NULL, remainder);
-
-	twr_gpio_set_output(PIN_CS, 1);
+		lcd_send_data(buffer, NULL, BUFFER_SIZE);
+	lcd_send_data(buffer, NULL, remainder);
 }
 
 void outline_screen(const uint16_t color)
@@ -196,12 +197,12 @@ void draw_pixel(uint16_t row, uint16_t col, uint16_t color)
 	draw_rect(col, col, row, row, color);
 }
 
-// draws any 8 byte bitmap, where each byte is a line
+// draws any 8 byte bitmap where each byte is a line
 void draw_char(unsigned char c, uint16_t grid_x, uint16_t grid_y, uint8_t text_size)
 {
 	const uint8_t *bitmap = font8x8_basic[c];
 
-	uint32_t byte_count = text_size * text_size * 2 * LETTER_EDGE * LETTER_EDGE;
+	uint32_t byte_count = text_size * text_size * 2 * LETTER_EDGE * LETTER_EDGE; // only supports up to text size 4 without chunked transfer
 	uint32_t buffer_index = 0;
 
 	for (uint8_t i = 0; i < 8 * text_size; i++)
@@ -226,11 +227,10 @@ void draw_char(unsigned char c, uint16_t grid_x, uint16_t grid_y, uint8_t text_s
 	const uint16_t row_start = text_size * grid_y * LETTER_EDGE;
 	const uint16_t row_end   = row_start + (text_size * LETTER_EDGE) - 1;
 
-	set_address(SET_COL, col_start, col_end);
-	set_address(SET_ROW, row_start, row_end);
-	start_pixel_stream();
-	twr_spi_transfer(buffer, NULL, byte_count);
-	twr_gpio_set_output(PIN_CS, 1);
+	lcd_set_col(col_start, col_end);
+	lcd_set_row(row_start, row_end);
+	lcd_pixel_stream();
+	lcd_send_data(buffer, NULL, byte_count);
 }
 
 // only needed when deleting, not for overwriting
@@ -255,7 +255,7 @@ void draw_image(uint16_t col, uint16_t row , uint8_t type)
 
 }
 
-// e.g. 136 Jizni Mesto
+// e.g. 136 Jizni Mesto; TODO: define memory regions for each part
 // void draw_headsign(char *s, )
 // {
 
