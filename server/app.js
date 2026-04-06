e1f646264c96df0a6d3be134632e3845880a3cd2
Adam Krejza <adamfkrejza@gmail.com>
2026-04-06 17:30:28 +0200

    Add towertest endpoint with real PID data and parsing
    This one is fully functional and can easily be modified

--- server/app.js
+++ server/app.js
@@ -183,6 +183,232 @@ app.use("/dbtest", async (req, res) => {
 	}
 });
 
+// test endpoint for a tower's TWO departures with correct parsing. based off /bustest
+app.get("/towertest", async (req, res) => {
+
+	// Based off the worked example in server-pid.md
+
+	//https://api.golemio.cz/v2/public/departureboards?stopIds={"0": ["U75Z101P"]}&stopIds={"1": ["U474Z6P"]}&limit=30&minutesAfter=60&minutesBefore=-15
+	// 	the metro B Zlicin at Kolbenova
+	//	gtfsId:U75Z101P
+	//	departureOffset: -15
+	//	the bus 177 Chodov at Vysocanska.
+	//	gtfsId: U474Z6P
+	//	departureOffset: -10
+
+	// use a JOIN to add the stop and line
+	const input = [
+		{
+			towerId: "547c65321d0b",
+			assignments: [
+				{
+					departureOffset: -15,
+					stopId: 1200, // generated in our database
+					lineId: 1, // generated in our DB
+					gtfsId: "U75Z101P", // from PID,
+					stop: {
+						id: 1200, // our db
+						slug: "kolbenova",
+						name: "Kolbenova",
+						displayAscii: "Kolbenova"
+					},
+					line: {
+						id: 1, // our db
+						pidId: 992, // from PID 
+						gtfsId: "U75Z101P",
+						name: "B", 
+						displayAscii: "Zlicin",
+						type: "metro", 
+						direction: "Zličín"
+					}
+				},
+				{
+					departureOffset: -10,
+					stopId: 3, // generated in our database
+					lineId: 54, // generated in our DB
+					gtfsId: "U474Z6P", // from PID,
+					stop: {
+						id: 3, // our db
+						slug: "vysocanska",
+						name: "Vysočanská",
+						displayAscii: "Vysocanska"
+					},
+					line: {
+						id: 54, // our db
+						pidId: 177, // from PID 
+						gtfsId: "U474Z6P",
+						name: "177", 
+						displayAscii: "Chodov",
+						type: "bus", 
+						direction: "Chodov"
+					}
+				}
+			]
+		}
+	];
+
+	// an array of assignments, each of which also has their towerId, means the index is the same as the response arrays from PID
+	const towerAssignments = [];
+	for (let i = 0; i < input.length; i++)
+	{
+		for (let j = 0; j < input[i].assignments.length; j++)
+		{
+			const assignment = input[i].assignments[j];
+			assignment.towerId = input[i].towerId;
+			towerAssignments.push(assignment);
+		}
+	}
+
+	let stopIds = [];
+	for (let i = 0; i < input.length; i++)
+	{
+		for (let j = 0; j < input[i].assignments.length; j++)
+		{
+			const stopId = input[i].assignments[j].gtfsId;
+			stopIds.push(stopId);
+		}
+	}
+	console.log(stopIds);
+
+	const stopIdParams = stopIds.map((id, i) => `stopIds={"${i}": ["${id}"]}`).join("&");
+	const minutesBefore = -15; // take lowest departureOffset
+	const minutesAfter = 60;
+	const limit = 30;
+	const url = `https://api.golemio.cz/v2/public/departureboards?${stopIdParams}&limit=${limit}&minutesAfter=${minutesAfter}&minutesBefore=${minutesBefore}`;
+	console.log(url);
+
+	const response = await fetch(url, {
+		method: "GET",
+		headers: {
+			"X-Access-Token": process.env.API_KEY
+		}
+	});
+
+
+	const allDepartures = await response.json();
+
+	// filter to correct ones then map to towerAssignments, then parse into correct structure
+	for (let i = 0; i < allDepartures.length; i++) {
+
+		// assign leaveIn and nextTime
+		for (let j = 0; j < allDepartures[i].length; j++) {
+			const name = towerAssignments[i].line.name;
+			const shortName = allDepartures[i][j].route.short_name;
+
+			const minutes = Number(allDepartures[i][j].departure.minutes);
+			const departureOffset = towerAssignments[i].departureOffset;
+			const leaveIn = minutes - departureOffset * -1;
+
+			if (leaveIn > 0 && name === shortName)
+			{
+				const predicted = allDepartures[i][j].departure.timestamp_predicted;
+				towerAssignments[i].nextTime = predicted.substring(11, 16);
+				towerAssignments[i].leaveIn = `${leaveIn}m`;
+				break;
+			}
+		}
+		// if no bus was found
+		if (!towerAssignments[i].nextTime)
+		{
+			towerAssignments[i].nextTime = `NONE`;
+			towerAssignments[i].leaveIn = `///`;
+		}
+	}
+
+	// everything MUST be a string!!!
+	class Assignment {
+		constructor(towerId, lineNumber, lineDirection, stopName, nextTime, leaveIn, type) {
+			this.towerId = towerId;
+			this.lineNumber = lineNumber;
+			this.lineDirection = lineDirection;
+			this.stopName = stopName;
+			this.nextTime = nextTime;
+			this.leaveIn = leaveIn;
+			this.type = type
+		}
+	}
+
+	const enumTransportTypes = {
+		bus: 0,
+		metro: 1,
+		tram: 2,
+		trolleybus: 3,
+		train: 4,
+		ferry: 5
+	}
+	// not including null byte
+	const LINE_NUMBER_SIZE = 3;
+	const LINE_DIRECTION_SIZE = 15;
+	const NEXT_TIME_SIZE = 5;
+	const LEAVE_IN_SIZE = 3;
+	const STOP_NAME_SIZE = 22;
+
+
+	const towersData = [];
+	// then shorten everything to the max sizes & set the type & clean up
+	for (let i = 0; i < towerAssignments.length; i++)
+	{
+		const towerId = towerAssignments[i].towerId;
+		const nextTime = towerAssignments[i].nextTime;
+		const leaveIn = towerAssignments[i].leaveIn;
+		let lineNumber;
+		let lineDirection;
+		let stopName;
+		const type = enumTransportTypes[towerAssignments[i].line.type.toLowerCase()];
+
+		if (towerAssignments[i].line.name.length > LINE_NUMBER_SIZE) {
+			lineNumber = towerAssignments[i].line.name.substring(0, LINE_NUMBER_SIZE);
+		} else lineNumber = towerAssignments[i].line.name;
+
+		if (towerAssignments[i].line.direction.length > LINE_DIRECTION_SIZE) {
+			lineDirection = towerAssignments[i].line.direction.substring(0, LINE_DIRECTION_SIZE - 2);
+			lineDirection = `${lineDirection}..`;
+		} else lineDirection = towerAssignments[i].line.direction;
+
+		if (towerAssignments[i].stop.displayAscii.length > STOP_NAME_SIZE) {
+			stopName = towerAssignments[i].stop.displayAscii.substring(0, STOP_NAME_SIZE - 2);
+			stopName = `${stopName}..`;
+		} else stopName = towerAssignments[i].stop.displayAscii;
+
+		const assignment = new Assignment(
+			towerId,
+			lineNumber,
+			lineDirection,
+			stopName,
+			nextTime,
+			leaveIn,
+			type
+		);
+		towersData.push(assignment);
+	}
+
+	console.log(towersData);
+
+	// parse it. This is very condensed so that every tower can be on the same topic
+	// write tower_001|136|Jizni Mesto|Vysocanska|15:50|10m|0|
+	// If a tower has 2 assignments, it must repeat the towerId.
+	// The string will never be that long anyway, ~600 bytes.
+
+	let assignments = "";
+	for (let i = 0; i < towersData.length; i++)
+	{
+		assignments = assignments.concat(towersData[i].towerId);
+		assignments = assignments.concat("|", towersData[i].lineNumber);
+		assignments = assignments.concat("|", towersData[i].lineDirection);
+		assignments = assignments.concat("|", towersData[i].stopName);
+		assignments = assignments.concat("|", towersData[i].nextTime);
+		assignments = assignments.concat("|", towersData[i].leaveIn);
+		assignments = assignments.concat("|", towersData[i].type);
+	}
+	assignments = assignments.concat("\n");
+
+	res.send(assignments);
+	
+	// final result will look something like
+	// 547c65321d0b|B|Zličín|Kolbenova|17:46|5m|1547c65321d0b|177|Chodov|Vysocanska|17:52|15m|0\n
+
+});
+
 app.listen(SERVER_PORT, () => {
 	console.log(`Server listening on port ${SERVER_PORT}`);
 });
